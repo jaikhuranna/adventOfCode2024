130997afbe01f9ce63473bcde5e68015f09557e9
Jai Khurana <64320051+jaikhuranna@users.noreply.github.com>
2024-12-08 20:24:06 +0530

    Day 1 Part 1 Solved

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -1,15 +1,49 @@
 import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Day1 {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Historian Hysteria");
-		File input = new File("/Users/jaikhurana/Developer/adventOfCode2024/inputday1puzzle1.txt");
+		ArrayList<Integer> firstList = new ArrayList<>();
+		ArrayList<Integer> secondList = new ArrayList<>();
+		File input = new File("/Users/jaikhurana/Developer/adventOfCode2024/Day1/inputday1puzzle1.txt");
 
 		// Read Input File
 		Scanner scanner = new Scanner(input);
+
+		//Parse into lists
 		while (scanner.hasNextLine()) {
-			System.out.println(scanner.nextLine());
+			String[] currLineArr = scanner.nextLine().split("   ");
+			firstList.add(Integer.parseInt(currLineArr[0]));
+			secondList.add(Integer.parseInt(currLineArr[1]));
 		}
+		// Sort the filds
+		firstList.sort(ascendingOrder);
+		secondList.sort(ascendingOrder);
+
+		System.out.println(calculateDistanceAndAdd(firstList, secondList));
+
+		scanner.close();
+	}
+
+	static Comparator<Integer> ascendingOrder = new Comparator<Integer>() {
+		@Override
+		public  int compare(Integer a, Integer b) 
+		{
+			return a - b;
+		}
+	}; 
+
+	static Integer calculateDistanceAndAdd(ArrayList<Integer> first, ArrayList<Integer> second)
+	{
+		Integer distanceSum = 0;
+		for (int i = 0; i < first.size(); i++) {
+			distanceSum += Math.abs(first.get(i) - second.get(i));
+		}
+		return distanceSum;
 	}
 }
